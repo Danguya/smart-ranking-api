3b655df4184bd1a09890628c0b0dfff226f85a5a
danguya <daniel.yava16@gmail.com>
2023-07-13 14:36:56 +0100

    chore: create validations

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -2,7 +2,6 @@
 import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
-import {v4 as uuid} from 'uuid'
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AtualizarJogadorDto } from './dtos/atualizar-jogador.dto';
